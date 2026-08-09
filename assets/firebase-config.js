7085f4c7c0aa3b1ebae9b3b751b92086b88b2d43
Altan <119978920+Alt4ns@users.noreply.github.com>
2026-08-09 21:47:58 +0300

    Pandi destek sistemi ve admin panelini ekle

---
+++ assets/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCiC4tWtVmT6-EztmBn4YrJzb2kP8nNQHg",
+  authDomain: "pandi-e0c6c.firebaseapp.com",
+  projectId: "pandi-e0c6c",
+  storageBucket: "pandi-e0c6c.firebasestorage.app",
+  appId: "1:433962265551:ios:dc503343709cad419b80ba"
+};
+
+export const adminEmail = "alt4ns@icloud.com";
